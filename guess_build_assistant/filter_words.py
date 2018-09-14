a6f9c41ab78a26a7346164eeafe90a97b26603f7
rlee287 <rlee287@yahoo.com>
2018-09-14 18:52:47 -0400

    Split only on newlines and refactor word matching code

--- guess_build_assistant/filter_words.py
+++ guess_build_assistant/filter_words.py
@@ -1,29 +1,38 @@
 from .hashable_lru import hashable_lru
 
 import functools
+
 def gen_wordlist(wordfile):
-    """Generates a list of words given a wordfile delimited by whitespace"""
+    """Generates a list of words given a line-delimited wordfile."""
     # read text in
     with open(wordfile,"r") as fil:
         text=fil.read()
-    # text.split splits the text into a list based on whitespace
-    return text.split()
+    # Split on lines but not spaces because of multiword phrases
+    return text.split("\n")
 
 # Cache repeated calls for checking the same word and pattern
 #@hashable_lru
 def filter_word(word, wordpattern):
-    """Checks if a word fits the wordpattern"""
+    """Checks if a word fits the wordpattern.
+       Special character mapping is performed as follows:
+       "-" -> " "
+       "*" -> <wildcard>"""
     if len(word)!=len(wordpattern):
         return False
     # Enumerate allows iteration over 
     for ind,char in enumerate(wordpattern):
         # Perform type checking to support string and char array wordpattern
         if isinstance(char,str):
-            char_comp=char
+            char_comp_pat=char
         else:
-            char_comp=chr(char)
-        if char_comp not in ["*",word[ind]]:
-            return False
+            char_comp_pat=chr(char)
+        # Handle special chars in wordpattern
+        char_comp_word=word[ind]
+        if char_comp_pat=="-":
+            char_comp_pat=" "
+        if char_comp_pat!="*":
+            if char_comp_pat!=char_comp_word:
+                return False
     return True
 
 
@@ -31,5 +40,5 @@ def filter_word(word, wordpattern):
 # Smaller cache as few calls will be made before reset
 #@hashable_lru
 def filter_wordlist(wordlist, wordpattern):
-    """Checks a word pattern against the wordlist"""
+    """Checks a word pattern against the wordlist."""
     return [word for word in wordlist if filter_word(word,wordpattern)]
